f741c83595edce6e3fe148c8c2282654ae80cce3
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-14 13:33:26 +0600

    project completed

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,14 @@
+function continueGame() {
+    // Step 1 : Generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    // set random generated alphabet to the screen(show)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    // set background color
+    addBackgroundColorById(alphabet);
+}
 function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,9 +1,25 @@
-function hideElementById(elementId){
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
-function showElementById(elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
+}
+
+function addBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+function getARandomAlphabet() {
+    // generate a random alphabet 
+    const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const randomIndex = Math.round(Math.random() * 25);
+    const alphabet = alphabets[randomIndex];
+    return alphabet;
 }
