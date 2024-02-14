293fac65aa99771d37676cf1bf9d14cba790f405
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-14 19:42:54 +0600

    Basic click event added to the website

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,17 @@
+function functionKeyPress(event) {
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    if (playerPressed === expectedAlphabet) {
+        console.log("You get a point")
+    }
+    else {
+        console.log('you missed. You lost a life');
+    }
+}
+document.addEventListener('keyup', functionKeyPress);
 function continueGame() {
     // Step 1 : Generate a random alphabet
     const alphabet = getARandomAlphabet();
