b52ce861c062b9411f486c6b52f432910adcbb7b
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-14 20:19:21 +0600

    Game beta completed

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -5,12 +5,22 @@ function functionKeyPress(event) {
     const expectedAlphabet = currentAlphabet.toLowerCase();
     if (playerPressed === expectedAlphabet) {
         console.log("You get a point");
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        const newScore = currentScore + 1;
+        currentScoreElement.innerText = newScore;
+        // start  new round
         console.log("You pressed correctly", expectedAlphabet);
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else {
-        console.log('you missed. You lost a life');
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        const newLife = currentLife - 1;
+        currentLifeElement.innerText = newLife;
     }
 }
 document.addEventListener('keyup', functionKeyPress);
